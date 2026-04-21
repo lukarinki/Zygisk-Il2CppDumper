671b9bb24599a82be4a94126e1b62fd780374ec5
lukarinki <50658342+lukarinki@users.noreply.github.com>
2026-04-21 22:00:35 +0700

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -46,7 +46,7 @@ private:
     size_t length;
 
     void preSpecialize(const char *package_name, const char *app_data_dir) {
-        if (strcmp(package_name, GamePackageName) == 0) {
+        if (strstr(process_name, GamePackageName) != nullptr) {
             LOGI("detect game: %s", package_name);
             enable_hack = true;
             game_data_dir = new char[strlen(app_data_dir) + 1];
